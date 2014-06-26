3e5ba6054156b7d6a543939db61f38bf8dcf15d9
Tomoko Kwan <tkwan@mdsol.com>
2014-06-26 16:58:02 +0900

    initial set of classes

---
+++ Medidata.ZipkinTracerModule/SpanCollector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift;
+
+namespace Medidata.ZipkinTracerModule
+{
+    public class SpanCollector
+    {
+        private BlockingCollection<Span> spanQueue;
+
+        private const int MAX_QUEUE_SIZE = 100;
+
+        public SpanCollector(IClientProvider clientProvider)
+        {
+            try
+            {
+                clientProvider.Setup();
+
+            }
+            catch (TException tEx)
+            {
+                clientProvider.Close();
+
+                throw new Exception("Error setting up connection to scribe", tEx);
+            }
+            
+            spanQueue = new BlockingCollection<Span>(MAX_QUEUE_SIZE);
+        }
+    }
+}
