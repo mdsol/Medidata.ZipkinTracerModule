d3524c6ecca6733a80bcb5c7d5db326f7795da18
Tomoko Kwan <tkwan@mdsol.com>
2014-06-26 14:56:51 +0900

    bat file correction

--- Medidata.ZipkinTracerModule/package-nuget.bat
+++ Medidata.ZipkinTracerModule/package-nuget.bat
@@ -1,2 +1,2 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "Medidata.MAuthAuthorizationModule.csproj" /p:Configuration=Release /t:Clean
-"..\.nuget\nuget.exe" pack Medidata.MAuthAuthorizationModule.csproj -Build -Properties Configuration=Release
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "Medidata.ZipkinTracerModule.csproj" /p:Configuration=Release /t:Clean
+"..\.nuget\nuget.exe" pack Medidata.ZipkinTracerModule.csproj -Build -Properties Configuration=Release
