cdb2bda7524082b2aa9b1de11ee377ab0da19053
Tomoko Kwan <tkwan@mdsol.com>
2014-07-10 21:31:14 -0700

    implement StartClientSpan

--- Medidata.ZipkinTracerModule.Test/SpanTracerTests.cs
+++ Medidata.ZipkinTracerModule.Test/SpanTracerTests.cs
@@ -11,28 +11,28 @@ namespace Medidata.ZipkinTracerModule.Test
     {
         private IFixture fixture;
         private SpanCollector spanCollectorStub;
-        private IZipkinEndpoint zipkinEndpoint;
+        private IZipkinEndpoint zipkinEndpointStub;
 
         [TestInitialize]
         public void Init()
         {
             fixture = new Fixture();
             spanCollectorStub = MockRepository.GenerateStub<SpanCollector>(MockRepository.GenerateStub<IClientProvider>());
-            zipkinEndpoint = MockRepository.GenerateStub<IZipkinEndpoint>();
+            zipkinEndpointStub = MockRepository.GenerateStub<IZipkinEndpoint>();
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void CTOR_WithNullSpanCollector()
         {
-            var spanTracer = new SpanTracer(null, fixture.Create<string>(), zipkinEndpoint);
+            var spanTracer = new SpanTracer(null, fixture.Create<string>(), zipkinEndpointStub);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void CTOR_WithNullOrEmptyString()
         {
-            var spanTracer = new SpanTracer(spanCollectorStub, null, zipkinEndpoint);
+            var spanTracer = new SpanTracer(spanCollectorStub, null, zipkinEndpointStub);
         }
 
         [TestMethod]
@@ -40,6 +40,37 @@ namespace Medidata.ZipkinTracerModule.Test
         public void CTOR_WithNullZipkinEndpoint()
         {
             var spanTracer = new SpanTracer(spanCollectorStub, fixture.Create<string>(), null);
+        }
+
+        [TestMethod]
+        public void StartClientSpan()
+        {
+            var serviceName = fixture.Create<string>();
+            var requestName = fixture.Create<string>();
+            var traceId = fixture.Create<long>().ToString();
+            var parentSpanId = fixture.Create<long>().ToString();
+
+            var spanTracer = new SpanTracer(spanCollectorStub, serviceName, zipkinEndpointStub);
+
+            zipkinEndpointStub.Expect(x => x.GetEndpoint(serviceName)).Return(new Endpoint() { Service_name = serviceName });
+
+            var resultSpan = spanTracer.StartClientSpan(requestName, traceId, parentSpanId);
+
+            Assert.AreEqual(requestName, resultSpan.Name);
+            Assert.AreEqual(traceId, resultSpan.Trace_id.ToString());
+            Assert.AreEqual(parentSpanId, resultSpan.Parent_id.ToString());
+
+            Assert.AreEqual(1, resultSpan.Annotations.Count);
+
+            var annotation = resultSpan.Annotations[0] as Annotation;
+            Assert.IsNotNull(annotation);
+            Assert.AreEqual(zipkinCoreConstants.CLIENT_SEND, annotation.Value);
+            Assert.IsNotNull(annotation.Timestamp);
+            Assert.IsNotNull(annotation.Host);
+
+            var endpoint = annotation.Host as Endpoint;
+            Assert.IsNotNull(endpoint);
+            Assert.AreEqual(serviceName, endpoint.Service_name);
         }
     }
 }
