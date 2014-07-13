21676824f1c41012d2047c29a2e5ae8a229fe8d8
Tomoko Kwan <tkwan@mdsol.com>
2014-07-12 19:28:31 -0700

    ZipkinEndpoint tests

---
+++ Medidata.ZipkinTracerModule.Test/ZipkinEndpointTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+namespace Medidata.ZipkinTracerModule.Test
+{
+    [TestClass]
+    public class ZipkinEndpointTests
+    {
+        private IFixture fixture;
+
+        [TestInitialize]
+        public void Init()
+        {
+            fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void GetEndpoint()
+        {
+            var serviceName = fixture.Create<string>();
+
+            var zipkinEndpoint = new ZipkinEndpoint();
+            var endpoint = zipkinEndpoint.GetEndpoint(serviceName);
+
+            Assert.IsNotNull(endpoint);
+            Assert.AreEqual(serviceName, endpoint.Service_name);
+            Assert.IsNotNull(endpoint.Ipv4);
+            Assert.IsNotNull(endpoint.Port);
+        }
+    }
+}
