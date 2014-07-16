7ca23916aa5ad9fa9f89a8a5ad94a2aa0cd462ec
Tomoko Kwan <tkwan@mdsol.com>
2014-07-15 23:05:24 -0700

    SpanProcessor and start test

---
+++ Medidata.ZipkinTracerModule.Collector.Test/SpanProcessorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+using System.Collections.Concurrent;
+using Rhino.Mocks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Medidata.ZipkinTracerModule.Collector.Test
+{
+    [TestClass]
+    public class SpanProcessorTests
+    {
+        private IFixture fixture;
+
+        [TestInitialize]
+        public void Init()
+        {
+            fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void Start()
+        {
+            var queue = new BlockingCollection<Span>();
+            var clientProvider = MockRepository.GenerateStub<IClientProvider>();
+            var spanProcessor = new SpanProcessor(queue, clientProvider);
+            var taskFactory = MockRepository.GenerateStub<SpanProcessorTaskFactory>();
+            spanProcessor.spanProcessorTaskFactory = taskFactory;
+
+            spanProcessor.Start();
+
+            Action givenAction = null;
+            taskFactory.Expect(x => x.CreateAndStart(Arg<Action>.Matches(y => ValidateAction(y, spanProcessor)), Arg<CancellationTokenSource>.Is.Anything));
+        }
+
+        private bool ValidateAction(Action y, SpanProcessor spanProcessor)
+        {
+            Assert.AreEqual(() => spanProcessor.LogSubmittedSpans(), y);
+            return true;
+        }
+    }
+}
--- Medidata.ZipkinTracerModule.Collector/SpanProcessor.cs
+++ Medidata.ZipkinTracerModule.Collector/SpanProcessor.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using Thrift;
 using Thrift.Protocol;
+using Thrift.Transport;
 
 namespace Medidata.ZipkinTracerModule.Collector
 {
@@ -12,25 +16,97 @@ namespace Medidata.ZipkinTracerModule.Collector
         private List<LogEntry> logEntries;
         private TBinaryProtocol.Factory protocolFactory;
 
+        private const int WAIT_INTERVAL_TO_DEQUEUE_MS = 1000;
+
         private int MAX_BATCH_SIZE = 20;
         private BlockingCollection<Span> spanQueue;
-        private IClientProvider cliendProvider;
+        private IClientProvider clientProvider;
 
-        public SpanProcessor(BlockingCollection<Span> spanQueue, IClientProvider cliendProvider)
+        private CancellationTokenSource cancellationTokenSource;
+        internal SpanProcessorTaskFactory spanProcessorTaskFactory;
+        private readonly BlockingCollection<Span> queue;
+
+        private int subsequentEmptyQueueCount;
+
+        public SpanProcessor(BlockingCollection<Span> spanQueue, IClientProvider clientProvider)
         {
             this.spanQueue = spanQueue;
-            this.cliendProvider = cliendProvider;
+            this.clientProvider = clientProvider;
             logEntries = new List<LogEntry>(MAX_BATCH_SIZE);
             protocolFactory = new TBinaryProtocol.Factory();
-        }
-
-        public virtual void Start()
-        {
+            spanProcessorTaskFactory = new SpanProcessorTaskFactory();
         }
 
         public virtual void Stop()
         {
-            throw new NotImplementedException();
+            if (cancellationTokenSource != null)
+            {
+                cancellationTokenSource.Cancel();
+            }
+        }
+
+        public virtual void Start()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            spanProcessorTaskFactory.CreateAndStart(() => LogSubmittedSpans(), cancellationTokenSource);
+        }
+
+        internal void LogSubmittedSpans()
+        {
+            while(!cancellationTokenSource.Token.IsCancellationRequested )
+            {
+                Span span;
+                queue.TryTake(out span, WAIT_INTERVAL_TO_DEQUEUE_MS);
+                if (span != null)
+                {
+                    logEntries.Add(Create(span));
+                    subsequentEmptyQueueCount = 0;
+                }
+                else
+                {
+                    subsequentEmptyQueueCount++;
+                }
+
+                //if (logEntries.Count() >= MAX_BATCH_SIZE 
+                //    || logEntries.Any() && cancellationTokenSource.Token.IsCancellationRequested
+                //    || logEntries.Any() && subsequentEmptyQueueCount > MAX_SUBSEQUENT_EMPTY_QUEUE)
+                if ( logEntries.Any() )
+                {
+                    //Log(clientProvider, logEntries);
+                    logEntries.Clear();
+                    subsequentEmptyQueueCount = 0;
+                }
+            } 
+        }
+
+        private void Log(ClientProvider client, List<LogEntry> logEntries)
+        {
+            try
+            {
+                //clientProvider.Client.Log(logEntries);
+            }
+            catch (TException tEx)
+            {
+                throw new Exception("Error writing to scribe", tEx);
+            }
+        }
+
+        private LogEntry Create(Span span)
+        {
+            var spanAsString = Convert.ToBase64String(ConvertSpanToBytes(span));
+            return new LogEntry()
+            {
+                Category = "zipkin",
+                Message = spanAsString
+            };
+        }
+
+        private byte[] ConvertSpanToBytes(Span span)
+        {
+            var buf = new MemoryStream();
+            TProtocol protocol = protocolFactory.GetProtocol(new TStreamTransport(buf, buf));
+            span.Write(protocol);
+            return buf.ToArray();
         }
     }
 }
