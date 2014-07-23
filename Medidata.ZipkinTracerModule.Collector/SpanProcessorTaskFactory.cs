232f2cdee9ccf971ff97f4744aa865fd56d4f7a1
Tomoko Kwan <tkwan@mdsol.com>
2014-07-23 16:46:39 +0900

    added ExcludeFromCodeCoverage attribute

--- Medidata.ZipkinTracerModule.Collector/SpanProcessorTaskFactory.cs
+++ Medidata.ZipkinTracerModule.Collector/SpanProcessorTaskFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,6 +10,7 @@ namespace Medidata.ZipkinTracerModule.Collector
 {
     public class SpanProcessorTaskFactory
     {
+        [ExcludeFromCodeCoverage]  //excluded from code coverage since this class is a 1 liner that starts up a background thread
         public virtual void CreateAndStart(Action action, CancellationTokenSource tokenSource)
         {
             new Task(action, tokenSource.Token, TaskCreationOptions.LongRunning).Start();
--- Medidata.ZipkinTracerModule/ZipkinConfig.cs
+++ Medidata.ZipkinTracerModule/ZipkinConfig.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 
 namespace Medidata.ZipkinTracerModule
 {
+    [ExcludeFromCodeCoverage]  //excluded from code coverage since this class are getters using static .net class ConfigurationManager
     public class ZipkinConfig : IZipkinConfig
     {
         public string ServiceName
