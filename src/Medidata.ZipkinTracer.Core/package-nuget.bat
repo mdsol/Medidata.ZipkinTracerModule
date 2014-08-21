64c391c06664ea6b274b1760c4bcf1ddc29948b3
Tomoko Kwan <tkwan@mdsol.com>
2014-08-21 11:09:21 +0900

    fix bat files command

--- src/Medidata.ZipkinTracer.Core/package-nuget.bat
+++ src/Medidata.ZipkinTracer.Core/package-nuget.bat
@@ -1,2 +1,2 @@
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "Medidata.ZipkinTracerModule.csproj" /p:Configuration=Release /t:Clean
-"..\.nuget\nuget.exe" pack Medidata.ZipkinTracer.Core.csproj -Build -Properties Configuration=Release
+"..\..\.nuget\nuget.exe" pack Medidata.ZipkinTracer.Core.csproj -Build -Properties Configuration=Release
