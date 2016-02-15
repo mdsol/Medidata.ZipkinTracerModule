a14e030b1087f768d9938ae85cc9d7fba1e484e3
Kenya Matsumoto <kmatsumoto@mdsol.com>
2016-02-15 15:04:31 +0900

    implement Middleware

---
+++ src/Medidata.ZipkinTracer.Core/Middlewares/ZipkinMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using log4net;
+using Microsoft.Owin;
+using Owin;
+
+namespace Medidata.ZipkinTracer.Core.Middlewares
+{
+    public class ZipkinMiddleware : OwinMiddleware
+    {
+        private readonly ZipkinMiddlewareOptions _options;
+
+        public ZipkinMiddleware(OwinMiddleware next, ZipkinMiddlewareOptions options) : base(next)
+        {
+            _options = options;
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            if (_options.Enable)
+            {
+                var traceProvider = new TraceProvider(context);
+                var logger = LogManager.GetLogger("ZipkinMiddleware");
+                var zipkin = new ZipkinClient(traceProvider, logger);
+                var span = zipkin.StartServerTrace(context.Request.Uri, context.Request.Method);
+                await Next.Invoke(context);
+                zipkin.EndServerTrace(span);
+            }
+        }
+    }
+
+    public static class AppBuilderExtensions
+    {
+        public static void UseZipkinAuthentication(this IAppBuilder app, ZipkinMiddlewareOptions options = null)
+        {
+            options = options ?? new ZipkinMiddlewareOptions();
+            app.Use<ZipkinMiddleware>(options);
+        }
+    }
+}
---
+++ src/Medidata.ZipkinTracer.Core/Middlewares/ZipkinMiddlewareOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Medidata.ZipkinTracer.Core.Middlewares
+{
+    public class ZipkinMiddlewareOptions
+    {
+        public ZipkinMiddlewareOptions()
+        {
+            Enable = true;
+        }
+
+        public bool Enable { get; set; }
+    }
+}
